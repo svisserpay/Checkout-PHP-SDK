f2a8287040d190ea49c87c19ff259c3f63e17aeb
Stefan Visser <stefan@pay.nl>
2021-10-19 16:32:44 +0200

    Fix small bug regarding uninitialized cache file

--- lib/PayPalCheckoutSdk/Cache/AuthorizationCache.php
+++ lib/PayPalCheckoutSdk/Cache/AuthorizationCache.php
@@ -57,7 +57,7 @@ class AuthorizationCache
         }
         
         // Reads all the existing persisted data
-        $tokens = self::pull() ?: array();
+        $tokens = self::pull() ?: (object)array();
         if (is_object($tokens)) {
             $tokens->{$key} = $accessTokenEncrypted->toCacheData();
         }
